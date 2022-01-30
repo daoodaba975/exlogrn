a75f000fb3f52e2ddec8fbc77a546abcab577e4c
daoodaba975 <davinhoba9@outlook.fr>
2022-01-30 01:29:49 +0000

    color theme ✨ config colors theme.

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#1e212b',
+    primary: '#ffc800',
+    secondary: '#1F2732',
+    error: '#ED1C24',
+  },
+}
